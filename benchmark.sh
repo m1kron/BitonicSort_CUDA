d7cda63938e0cb41b35d70aa20025d0965d512f8
m1kron <piotr.kowalczyk102+github@gmail.com>
2020-08-12 13:29:49 +0200

    [BuildScripts]: Refactoring.

--- benchmark.sh
+++ benchmark.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-../buildTEMP/proto1
+../buildTEMP/BitonicSort
--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 rm -rf ../buildTEMP
 mkdir ../buildTEMP
 cd ../buildTEMP
-cmake ../proto1 -DCMAKE_BUILD_TYPE=Release $@
+cmake ../BitonicSort -DCMAKE_BUILD_TYPE=Release $@
 make
